bbc08432bc0e04393864f07c8c09e64a60f836eb
Shubhansh16 <shubhansh77@gmail.com>
2023-09-06 16:12:55 +0530

    "added storage"

--- app/src/main/java/com/example/instamin/CreateActivity.kt
+++ app/src/main/java/com/example/instamin/CreateActivity.kt
@@ -1,11 +1,128 @@
 package com.example.instamin
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import com.example.instamin.models.Post
+import com.example.instamin.models.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
+private const val TAG ="CreateActivity"
+private const val PICK_PHOTO_CODE = 1234
 class CreateActivity : AppCompatActivity() {
+
+    private lateinit var submitBtn:Button
+    private var signedInUser:User?=null
+    private lateinit var firestoreDb: FirebaseFirestore
+    private lateinit var btnPickImage:Button
+    private lateinit var imageview:ImageView
+    private lateinit var etDescription:EditText
+    private var photoUri:Uri?=null
+    private lateinit var storageReference: StorageReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+
+        btnPickImage=findViewById(R.id.chooseBtn)
+        imageview=findViewById(R.id.imageView)
+        etDescription=findViewById(R.id.etDescription)
+        submitBtn=findViewById(R.id.btnSubmit)
+
+        storageReference=FirebaseStorage.getInstance().reference
+        firestoreDb= FirebaseFirestore.getInstance()
+
+        firestoreDb.collection("users")
+            .document(FirebaseAuth.getInstance().currentUser?.uid as String)
+            .get()
+            .addOnSuccessListener { userSnapshot->
+                signedInUser = userSnapshot.toObject(User::class.java)
+                Log.i(TAG,"signed in user: $signedInUser")
+            }
+
+            .addOnFailureListener { exception ->
+                Log.i(TAG,"failure fetching signed in user",exception)
+            }
+
+        submitBtn.setOnClickListener {
+            handleSubmitButtonClick()
+        }
+
+        btnPickImage.setOnClickListener {
+            Log.i(TAG,"open up image picker in device")
+            val imagePickerIntent = Intent(Intent.ACTION_GET_CONTENT)
+            imagePickerIntent.type ="image/*"
+            if (imagePickerIntent.resolveActivity(packageManager) !=null) {
+                startActivityForResult(imagePickerIntent, PICK_PHOTO_CODE)
+            }
+        }
+    }
+
+    private fun handleSubmitButtonClick() {
+        if (photoUri == null){
+            Toast.makeText(this, "No photo selected", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (etDescription.text.isBlank()) {
+            Toast.makeText(this, "Description cannot be empty", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (signedInUser ==null){
+            Toast.makeText(this, "No signed in user", Toast.LENGTH_SHORT).show()
+            return
+        }
+        submitBtn.isEnabled= false
+
+        val photoUploadUri =photoUri as Uri
+        val photoReference = storageReference.child("images/${System.currentTimeMillis()}-photo.jpg")
+
+        photoReference.putFile(photoUploadUri)
+            .continueWithTask { photoUploadTask ->
+             Log.i(TAG,"uploaded bytes ${photoUploadTask.result?.bytesTransferred}")
+                 photoReference.downloadUrl
+            }.continueWithTask { downloadUrlTask ->
+                val post = Post(
+                    etDescription.text.toString(),
+                    downloadUrlTask.result.toString(),
+                    System.currentTimeMillis(),
+                    signedInUser)
+                firestoreDb.collection("posts").add(post)
+
+            }.addOnCompleteListener { postCreationTask ->
+                submitBtn.isEnabled = true
+                if (!postCreationTask.isSuccessful) {
+                    Log.e(TAG,"Exception during firebase operation", postCreationTask.exception)
+                    Toast.makeText(this, "Failed to save post", Toast.LENGTH_SHORT).show()
+                }
+                etDescription.text.clear()
+                imageview.setImageResource(0)
+                Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+                val profileIntent = Intent(this, ProfileActivity::class.java)
+                profileIntent.putExtra(EXTRA_USERNAME,signedInUser?.username)
+                startActivity(profileIntent)
+                finish()
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == PICK_PHOTO_CODE){
+            if (resultCode== Activity.RESULT_OK){
+               photoUri= data?.data
+                imageview.setImageURI(photoUri)
+                Log.i(TAG,"photoUri $photoUri")
+            } else {
+                Toast.makeText(this, "Image pick action canceled", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/instamin/MainActivity.kt
+++ app/src/main/java/com/example/instamin/MainActivity.kt
@@ -25,78 +25,78 @@ open class MainActivity : AppCompatActivity() {
     private lateinit var posts: MutableList<Post>
     private lateinit var rvPosts: RecyclerView
     private lateinit var adapter: PostsAdapter
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        fabCreate= findViewById(R.id.floatingBtn)
-        //create a ui for the rv - done
-        //create an adapter
-        //create a data source- done
-        posts= mutableListOf()
-        //bind the adapter and layout manager to the rv
-        adapter= PostsAdapter(this,posts)
-        rvPosts= findViewById(R.id.rvPosts)
-        rvPosts.adapter= adapter
-        rvPosts.layoutManager= LinearLayoutManager(this)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
 
-        firestoreDb= FirebaseFirestore.getInstance()
+            rvPosts=findViewById(R.id.rvPosts)
+
+            // Create the layout file which represents one post - DONE
+            // Create data source - DONE
+            posts = mutableListOf()
+            fabCreate=findViewById(R.id.floatingBtn)
+            // Create the adapter
+            adapter = PostsAdapter(this, posts)
+            // Bind the adapter and layout manager to the RV
+            rvPosts.adapter = adapter
+            rvPosts.layoutManager = LinearLayoutManager(this)
+            firestoreDb = FirebaseFirestore.getInstance()
+
+            firestoreDb.collection("users")
+                .document(FirebaseAuth.getInstance().currentUser?.uid as String)
+                .get()
+                .addOnSuccessListener { userSnapshot ->
+                    signedInUser = userSnapshot.toObject(User::class.java)
+                    Log.i(TAG, "signed in user: $signedInUser")
+                }
+                .addOnFailureListener { exception ->
+                    Log.i(TAG, "Failure fetching signed in user", exception)
+                }
 
 
-        firestoreDb.collection("users")
-            .document(FirebaseAuth.getInstance().currentUser?.uid as String)
-            .get()
-            .addOnSuccessListener {
-                signedInUser = it.toObject(User::class.java)
-                Log.i(TAG,"signed in user: $signedInUser")
+            var postsReference = firestoreDb
+                .collection("posts")
+                .limit(20)
+                .orderBy("creation_time_ms", Query.Direction.DESCENDING)
+
+            val username = intent.getStringExtra(EXTRA_USERNAME)
+            if (username != null) {
+                supportActionBar?.title = username
+                postsReference = postsReference.whereEqualTo("user.username", username)
             }
 
-            .addOnFailureListener { exception ->
-                Log.i(TAG,"failure fetching signed in user",exception)
+            postsReference.addSnapshotListener { snapshot, exception ->
+                if (exception != null || snapshot == null) {
+                    Log.e(TAG, "Exception when querying posts", exception)
+                    return@addSnapshotListener
+                }
+                val postList = snapshot.toObjects(Post::class.java)
+                posts.clear()
+                posts.addAll(postList)
+                adapter.notifyDataSetChanged()
+                for (post in postList) {
+                    Log.i(TAG, "Post ${post}")
+                }
             }
 
-        var postsReference = firestoreDb
-            .collection("posts")
-            .limit(20)
-            .orderBy("creation_time_ms", Query.Direction.DESCENDING)
-
-        val username = intent.getStringExtra(EXTRA_USERNAME)
-        if (username!=null){
-            supportActionBar?.title = username
-            postsReference= postsReference.whereEqualTo("user.username",username)
-        }
-
-        postsReference.addSnapshotListener { snapshot, exception ->
-           if (exception!=null || snapshot == null){
-               Log.e(TAG,"exception when querying posts",exception)
-               return@addSnapshotListener
-           }
-
-            val postList = snapshot.toObjects(Post::class.java)
-            posts.clear()
-            posts.addAll(postList)
-            adapter.notifyDataSetChanged()
-            for (post in postList) {
-                Log.i(TAG,"Post ${post}")
+            fabCreate.setOnClickListener {
+                val intent = Intent(this, CreateActivity::class.java)
+                startActivity(intent)
             }
         }
-        fabCreate.setOnClickListener{
-            val intent = Intent(this, CreateActivity::class.java)
-            startActivity(intent)
+
+        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+            menuInflater.inflate(R.menu.menu_posts, menu)
+            return super.onCreateOptionsMenu(menu)
+        }
+
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            if (item.itemId == R.id.profile) {
+                val intent = Intent(this, ProfileActivity::class.java)
+                intent.putExtra(EXTRA_USERNAME,"jethalal")
+                startActivity(intent)
+            }
+            return super.onOptionsItemSelected(item)
         }
     }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_posts,menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId== R.id.profile) {
-           val intent = Intent(this, ProfileActivity::class.java)
-            intent.putExtra(EXTRA_USERNAME ,"jethalal")
-            startActivity(intent)
-        }
-        return super.onOptionsItemSelected(item)
-    }
-}
